c74a2a5307662a71e6ffea95f347630accfc0148
Grini Youssef <grini.youssef@gmail.com>
2021-01-02 13:47:26 +0100

    added swagger

--- src/main/java/com/grini/investisement/EInvestisementApplication.java
+++ src/main/java/com/grini/investisement/EInvestisementApplication.java
@@ -2,10 +2,14 @@ package com.grini.investisement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import com.grini.investisement.config.SwaggerConfiguration;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class EInvestisementApplication {
 
 	public static void main(String[] args) {
